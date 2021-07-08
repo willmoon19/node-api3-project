c9570b654e5b245d866458560171c1cbd430d9fd
William Moon <william-moon@lambdastudents.com>
2021-07-07 22:00:39 -0700

    heroku deploy

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // require your server and launch it
+require('dotenv').config();
+
 const server = require('./api/server');
 
-const PORT = 5000
+const port = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
-    console.log('listneing on port 5000');
+server.listen(port, () => {
+    console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 })
